6686edd05529a1e4860adf8be5b6b293f0580c9b
Jack Tracey <jack@jacktracey.co.uk>
2024-04-24 14:40:37 +0100

    working

--- tools/jt/recommendationReporter.py
+++ tools/jt/recommendationReporter.py
@@ -1,7 +1,10 @@
 import os
 import glob
 from termcolor import colored, cprint
-# import yaml
+import yaml
+import json
+import pandas as pd
+import xlsxwriter
 
 # *** Functions ***
 # **Find all recommendations.yaml files in the azure-resources directory and child folders**
@@ -79,5 +82,81 @@ for azure_rp_w_type in out_azure_rps_and_types:
     index += 1
     print(colored(f'{index}: Microsoft.{azure_rp_w_type}', 'light_yellow'))
 
+# for each recommendation.yaml file, import the yaml and filter out the recommendations that are not recommendationImpact: High and pgVerified: true
 
+# Assuming 'out_get_recommendations' contains relative paths to the recommendation files
+azure_resources_dir = os.path.join('..', '..', 'azure-resources')  # Path to the azure-resources directory
+# Normalize the path to remove any redundant '\.\'
+azure_resources_dir = os.path.normpath(azure_resources_dir)
+total_number_of_recommendations = 0
+total_number_of_high_impact_recommendations = 0
+total_number_of_pg_verified_recommendations = 0
+total_number_of_high_impact_and_pg_verified_recommendations = 0
+high_impact_and_pg_verified_recommendations = {}
+
+for recommendation in out_get_recommendations:
+    recommendation_path = os.path.join(azure_resources_dir, recommendation)  # Constructs an absolute path to the recommendation file
+    recommendation_path = os.path.normpath(recommendation_path)  # Normalize the path to ensure it's correctly formatted
+    try:
+        with open(recommendation_path, 'r', encoding='utf-8-sig') as file:
+            # Load the file as a list of dictionaries
+            recommendations_data = yaml.safe_load(file)
+            print(colored(f'\n{recommendation_path} has {len(recommendations_data)} recommendations in total', 'light_green'), end='\n')
+            # Check if recommendations_data is a list and iterate over it
+            if isinstance(recommendations_data, list):
+                for rec in recommendations_data:
+                    total_number_of_recommendations += 1
+                    if rec.get('recommendationImpact') == 'High':
+                        total_number_of_high_impact_recommendations += 1
+                    if rec.get('pgVerified') == True:
+                        total_number_of_pg_verified_recommendations += 1
+                    if rec.get('recommendationImpact') == 'High' and rec.get('pgVerified') == True:
+                        total_number_of_high_impact_and_pg_verified_recommendations += 1
+                        high_impact_and_pg_verified_recommendations.update({rec["aprlGuid"]: {"recommendationResourceType": rec["recommendationResourceType"], "description": rec["description"], "publishedToLearn": rec["publishedToLearn"], "publishedToAdvisor": rec["publishedToAdvisor"], "automationAvailable": rec["automationAvailable"]}})
+            else:
+                print(f"Unexpected data structure in {recommendation_path}: {type(recommendations_data)}")
+    except FileNotFoundError:
+        print(f"File not found: {recommendation_path}")
+
+df = pd.DataFrame(data=high_impact_and_pg_verified_recommendations)
+df = df.T  # Transpose the DataFrame
+writer = pd.ExcelWriter('aprlPgVerifiedAndHighImpactRecommendations.xlsx', engine='xlsxwriter')
+df.to_excel(writer, sheet_name='APRL High Impact & PG Verified', index=False)
+
+workbook = writer.book
+worksheet = writer.sheets['APRL High Impact & PG Verified']
+(max_row, max_col) = df.shape
+
+# Create a center alignment format
+center_format = workbook.add_format({'align': 'center'})
+
+# Apply center alignment format to all columns except the first two
+worksheet.set_column(2, max_col - 1, None, center_format)  # Start from the third column
+
+# Define the table with column settings
+column_settings = [{"header": column} for column in df.columns]
+worksheet.add_table(0, 0, max_row, max_col - 1, {"columns": column_settings})
+
+# Autofit the columns
+worksheet.autofit()
+
+# Close the Pandas Excel writer and output the Excel file
+writer.close()
+
+# high_impact_vs_total_percentage = round((total_number_of_high_impact_recommendations/total_number_of_recommendations)*100, 2)
+# pg_verified_vs_total_percentage = round((total_number_of_pg_verified_recommendations/total_number_of_recommendations)*100, 2)
+# pg_verified_vs_high_impact_percentage = round((total_number_of_high_impact_and_pg_verified_recommendations/total_number_of_high_impact_recommendations)*100, 2)
+# pg_verified_and_high_impact_vs_total_percentage = round((total_number_of_high_impact_and_pg_verified_recommendations/total_number_of_recommendations)*100, 2)
+# print(high_impact_vs_total_percentage)
+# print(pg_verified_vs_total_percentage)
+# print(pg_verified_vs_high_impact_percentage)
+# print(pg_verified_and_high_impact_vs_total_percentage)
+# print(total_number_of_recommendations)
+# print(len(high_impact_and_pg_verified_recommendations))
+# print(total_number_of_high_impact_and_pg_verified_recommendations)
+# print(high_impact_and_pg_verified_recommendations)
+# print()
+# print(json.dumps(high_impact_and_pg_verified_recommendations))
+# print(total_number_of_high_impact_recommendations)
+# print(total_number_of_pg_verified_recommendations)
 
