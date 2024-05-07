26a8c49edb8d9f0b267c10dbb6e6a6c3425048e3
Kyle Poineal <38540295+kpoineal@users.noreply.github.com>
2024-05-07 15:36:22 -0600

    ADD: Validate-kql-comments.ps1

---
+++ .github/scripts/validate-kql-comments.ps1
@@ -0,0 +1,48 @@
+#Functions
+Function Validate-KqlComments([array]$filearray) {
+$kqlresults = @()
+ForEach($file in $filearray){
+
+  #Test if the kql isn't actually part of the APRL recommendations and skip it.
+  if($file.name -like "*replace*"){Continue}
+
+  #Create psobject scaffolding
+  $kqlresult = [PSCustomObject]@{
+    FileName = $file.FullName
+    Result = ""
+    Description = ""
+  }
+
+  #Get the first two lines of the kql file
+  $content = Get-Content $file.FullName -TotalCount 2
+
+  #if($content[0] -like "//*under*" -or $content[0] -like "//*cannot*" -or $content[0] -like "//*under-*" -or $content[0] -eq "// under-development")
+  if ($content[0] -match "^//.*(under|cannot)"){
+    $kqlresult.Result = "N/A"
+    $kqlresult.Description = "There is no query associated with this recommendation."
+  }
+  elseif($content[0] -ne "// Azure Resource Graph Query"){
+    $kqlresult.Result = "FAIL"
+    $kqlresult.Description = "The first line of the KQL file is not a comment with 'Azure Resource Graph Query'. It should be a comment with 'Azure Resource Graph Query'."
+  }
+  elseif ($content[1] -notlike "// *"){
+    $kqlresult.Result = "FAIL"
+    $kqlresult.Description = "The second line of the KQL file is not a comment. It should be a comment with a description of the query."
+  }
+  else{
+    $kqlresult.Result = "PASS"
+    $kqlresult.Description = "The first two lines of the KQL file are comments."
+  }
+  $kqlresults += $kqlresult
+  }
+
+  return $kqlresults
+}
+
+
+
+$kqlfiles = Get-ChildItem -Path . -Filter *.kql -Recurse
+
+$test = Validate-KqlComments($kqlfiles)
+
+$test | Where {$_.Result -eq "FAIL"}
