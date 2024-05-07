37c870105ef66d32dc6921e469789ff13b90b3db
Kyle Poineal <38540295+kpoineal@users.noreply.github.com>
2024-05-07 15:48:20 -0600

    FIX: Adjusted validate-kql-comments to work with weird encoding issue.

--- .github/scripts/validate-kql-comments.ps1
+++ .github/scripts/validate-kql-comments.ps1
@@ -11,23 +11,27 @@ ForEach($file in $filearray){
     FileName = $file.FullName
     Result = ""
     Description = ""
+    CommentString = ""
   }
 
   #Get the first two lines of the kql file
   $content = Get-Content $file.FullName -TotalCount 2
 
   #if($content[0] -like "//*under*" -or $content[0] -like "//*cannot*" -or $content[0] -like "//*under-*" -or $content[0] -eq "// under-development")
-  if ($content[0] -match "^//.*(under|cannot)"){
+  if ($content[0] -match "^//.*(under|cannot)" -or $content -match "^//.*(under|cannot)"){
     $kqlresult.Result = "N/A"
     $kqlresult.Description = "There is no query associated with this recommendation."
+    $kqlresult.CommentString = $content
   }
   elseif($content[0] -ne "// Azure Resource Graph Query"){
     $kqlresult.Result = "FAIL"
     $kqlresult.Description = "The first line of the KQL file is not a comment with 'Azure Resource Graph Query'. It should be a comment with 'Azure Resource Graph Query'."
+    $kqlresult.CommentString = $content
   }
   elseif ($content[1] -notlike "// *"){
     $kqlresult.Result = "FAIL"
     $kqlresult.Description = "The second line of the KQL file is not a comment. It should be a comment with a description of the query."
+    $kqlresult.CommentString = $content
   }
   else{
     $kqlresult.Result = "PASS"
