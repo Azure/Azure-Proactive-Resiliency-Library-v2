02c4541250ab83315f1687b347c3f72e45d9776e
Zach Trocinski <ztrocinski@outlook.com>
2024-05-14 12:07:09 -0500

    Adding logging for directory

--- .github/scripts/validate-yaml-recommendations.py
+++ .github/scripts/validate-yaml-recommendations.py
@@ -61,6 +61,9 @@ print("Test Message: Validating YAML files against schemas")
 # Loop through directories
 for directory, schema_path in directories.items():
     print(f"Validating YAML files in directory: {directory}")
+    if not os.path.exists(directory):
+        print(f"Directory {directory} does not exist.")
+        continue
     for root, dirs, files in os.walk(directory):
         print(f"Validating YAML files in directory: {root}")
         for file in files:
