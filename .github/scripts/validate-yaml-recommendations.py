c078f5995019d614dea2a9900c50ede37f8ba761
Zach Trocinski <ztrocinski@outlook.com>
2024-05-14 12:26:17 -0500

    Fix schema directory path

--- .github/scripts/validate-yaml-recommendations.py
+++ .github/scripts/validate-yaml-recommendations.py
@@ -38,9 +38,9 @@ init()
 
 # Directories containing YAML files to validate
 directories = {
-    './azure-resources': '/schemas/azure-resources-schema.yaml',
-    './azure-specialized-workloads': '/schemas/azure-specialized-workloads-schema.yaml',
-    './azure-waf': '/schemas/azure-waf-schema.yaml'
+    './azure-resources': './.github/schemas/azure-resources-schema.yaml',
+    './azure-specialized-workloads': './.github/schemas/azure-specialized-workloads-schema.yaml',
+    './azure-waf': './.github/schemas/azure-waf-schema.yaml'
 }
 
 # Function to validate a YAML file against the schema
