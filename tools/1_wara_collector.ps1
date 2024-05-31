e90da7cadcd0b64a641f5845e52b103a607e6ca1
Kyle Poineal <38540295+kpoineal@users.noreply.github.com>
2024-05-31 10:02:27 -0600

    FIX: Added -first 1000 to azgraph queries.

--- tools/1_wara_collector.ps1
+++ tools/1_wara_collector.ps1
@@ -49,9 +49,9 @@ $Script:Runtime = Measure-Command -Expression {
     )
 
     if ([bool]$subscriptionId) {
-      $result = Search-AzGraph -Query $query -SkipToken $result.SkipToken -Subscription $subscriptionId
+      $result = Search-AzGraph -Query $query -SkipToken $result.SkipToken -Subscription $subscriptionId -First 1000
     } else {
-      $result = Search-AzGraph -Query $query -SkipToken $result.SkipToken
+      $result = Search-AzGraph -Query $query -SkipToken $result.SkipToken -first 1000
     } # -first 1000 returns the first 1000 results and subsequently reduces the amount of queries required to get data.
 
     # Collection to store all resources
@@ -61,9 +61,9 @@ $Script:Runtime = Measure-Command -Expression {
     while ($result.SkipToken) {
       # Retrieve the next set of results using the skip token
       if ([bool]$subscriptionId) {
-        $result = Search-AzGraph -Query $query -SkipToken $result.SkipToken -Subscription $subscriptionId
+        $result = Search-AzGraph -Query $query -SkipToken $result.SkipToken -Subscription $subscriptionId -First 1000
       } else {
-        $result = Search-AzGraph -Query $query -SkipToken $result.SkipToken
+        $result = Search-AzGraph -Query $query -SkipToken $result.SkipToken -First 1000
       }
       # Add the results to the collection
       $allResources += $result
