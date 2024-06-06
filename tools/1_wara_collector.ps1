1ad170ca85cb5b5a6cb5b08b051afe7d931b6b7e
Kyle Poineal <38540295+kpoineal@users.noreply.github.com>
2024-06-06 07:03:02 -0600

    ADD: SubIds in connect-toAzure

--- tools/1_wara_collector.ps1
+++ tools/1_wara_collector.ps1
@@ -245,7 +245,7 @@ $Script:Runtime = Measure-Command -Expression {
 
   function Connect-ToAzure {
     # Connect To Azure Tenant
-    Write-Host "Authenticating to Azure"
+<#     Write-Host "Authenticating to Azure"
     if ($Script:ShellPlatform -eq 'Win32NT')
       {
         Clear-AzContext -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
@@ -283,7 +283,9 @@ $Script:Runtime = Measure-Command -Expression {
       {
         Connect-AzAccount -Identity -Environment $AzureEnvironment
         $Script:SubIds = Get-AzSubscription -WarningAction SilentlyContinue
-      }
+      } #>
+
+      $Script:SubIds = Get-AzSubscription -TenantId $TenantID -WarningAction SilentlyContinue
 
     # Getting Outages
     Write-Debug "Exporting Outages"
