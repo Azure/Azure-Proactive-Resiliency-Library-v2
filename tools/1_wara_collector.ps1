e1903822d6cd5d5e91e8f125cec0eb6527ad4acc
Kyle Poineal <38540295+kpoineal@users.noreply.github.com>
2024-06-17 10:53:19 -0600

    ADD: Functions to main script body for GUI and filtering

--- tools/1_wara_collector.ps1
+++ tools/1_wara_collector.ps1
@@ -47,11 +47,11 @@ $Script:Runtime = Measure-Command -Expression {
 
   Function Get-AllAzGraphResource {
     param (
-      [string]$subscriptionId,
+      [string[]]$subscriptionId,
       [string]$query = 'Resources | project id, resourceGroup, subscriptionId, name, type, location'
     )
 
-    $result = Search-AzGraph -Query $query -first 1000 -Subscription $subscriptionId -ErrorAction SilentlyContinue # -first 1000 returns the first 1000 results and subsequently reduces the amount of queries required to get data.
+    $result = $subscriptionId ? (Search-AzGraph -Query $query -first 1000 -Subscription $subscriptionId) : (Search-AzGraph -Query $query -first 1000 -usetenantscope) # -first 1000 returns the first 1000 results and subsequently reduces the amount of queries required to get data.
 
     # Collection to store all resources
     $allResources = @($result)
@@ -59,7 +59,7 @@ $Script:Runtime = Measure-Command -Expression {
     # Loop to paginate through the results using the skip token
     while ($result.SkipToken) {
       # Retrieve the next set of results using the skip token
-      $result = Search-AzGraph -Query $query -SkipToken $result.SkipToken -Subscription $subscriptionId -First 1000 -ErrorAction SilentlyContinue
+      $result = $subscriptionId ? (Search-AzGraph -Query $query -SkipToken $result.SkipToken -Subscription $subscriptionId -First 1000) : (Search-AzGraph -query $query -SkipToken $result.SkipToken -First 1000 -UseTenantScope)
       # Add the results to the collection
       $allResources += $result
     }
@@ -68,19 +68,101 @@ $Script:Runtime = Measure-Command -Expression {
     return $allResources
   }
 
-  function Get-AllResourceGroup {
 
-    # Query to get all resource groups in the tenant
-    $q = "resourcecontainers
-    | where type == 'microsoft.resources/subscriptions'
-    | project subscriptionId, subscriptionName = name
-    | join (resourcecontainers
-        | where type == 'microsoft.resources/subscriptions/resourcegroups')
-        on subscriptionId
-    | project subscriptionName, subscriptionId, resourceGroup, id=tolower(id)"
+function Get-AllResourceGroup {
+  param (
+    [string[]]$SubscriptionIds
+  )
 
-    return Get-AllAzGraphResource -query $q
+  # Query to get all resource groups in the tenant
+  $q = "resourcecontainers
+  | where type == 'microsoft.resources/subscriptions'
+  | project subscriptionId, subscriptionName = name
+  | join (resourcecontainers
+      | where type == 'microsoft.resources/subscriptions/resourcegroups')
+      on subscriptionId
+  | project subscriptionName, subscriptionId, resourceGroup, id=tolower(id)"
+
+  $r = $SubscriptionIds ? (Get-AllAzGraphResource -query $q -subscriptionId $SubscriptionIds -usetenantscope) : (Get-AllAzGraphResource -query $q -usetenantscope)
+
+  # Returns the resource groups
+  return $r
+}
+
+  function New-AzTenantSelection {
+    return Get-AzTenant | Out-ConsoleGridView -OutputMode Single -Title "Select Tenant"
   }
+
+  function New-AzSubscriptionSelection {
+    param (
+      [Parameter(Mandatory=$true)]
+      [string]$TenantId
+    )
+    return Get-AzSubscription -TenantId $TenantId | Out-ConsoleGridView -OutputMode Multiple -title "Select Subscription(s)"
+  }
+
+  function New-AzResourceGroupSelection {
+    param (
+      [Parameter(Mandatory=$true)]
+      [string[]]$SubscriptionIds
+    )
+    return Get-AllResourceGroup -SubscriptionId $SubscriptionIds | Select-Object ResourceGroup, SubscriptionName, resourceId | Out-ConsoleGridView -OutputMode Multiple -Title "Select Resource Group(s)"
+  }
+
+  function Import-ConfigFileData($file){
+    # Read the file content and store it in a variable
+    $filecontent = (Get-content $file).trim().tolower()
+
+    # Create an array to store the line number of each section
+    $linetable = @()
+    $objarray = @{}
+
+    # Iterate through the file content and store the line number of each section
+    Foreach($line in $filecontent){
+        if (-not [string]::IsNullOrWhiteSpace($line) -and -not $line.startswith("#")){
+            # If the line is a section, store the line number
+            if ($line -match "^\[([^\]]+)\]$") {
+                # Store the section name and line number. Remove the brackets from the section name
+                $linetable += $filecontent.indexof($line)
+
+            }
+        }
+    }
+
+    # Iterate through the line numbers and extract the section content
+    $count = 0
+    foreach($entry in $linetable){
+
+        # Get the section name
+        $name = $filecontent[$entry]
+        # Remove the brackets from the section name
+        $name = $name.replace("[","").replace("]","")
+
+        # Get the start and stop line numbers for the section content
+        # If the section is the last one, set the stop line number to the end of the file
+        $start = $entry + 1
+        if($count -eq ($linetable.length-1)){
+            $stop = $filecontent.length - 1
+        }
+        else{
+            $stop = $linetable[$count+1] - 2
+        }
+
+        # Extract the section content
+        $configsection = $filecontent[$start..$stop]
+
+        # Add the section content to the object array
+        $objarray += @{$Name=$configsection}
+
+        # Increment the count
+        $count++
+    }
+
+    # Return the object array and cast to pscustomobject
+    return [pscustomobject]$objarray
+
+  }
+
 
   function Get-ResourceGroupsByList {
     param (
