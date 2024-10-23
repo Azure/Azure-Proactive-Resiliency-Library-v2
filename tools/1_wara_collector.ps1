54db588e6fe1e06377045d6e9ddc30fd32896822
Kyle Poineal <38540295+kpoineal@users.noreply.github.com>
2024-10-23 08:48:26 -0600

    Update: Import-ConfigFileData

--- tools/1_wara_collector.ps1
+++ tools/1_wara_collector.ps1
@@ -218,59 +218,60 @@ $Script:Runtime = Measure-Command -Expression {
     return $r
   }
 
-  function Import-ConfigFileData($file){
+  function Import-ConfigFileData($file) {
     # Read the file content and store it in a variable
+    $filecontent,$linetable,$objarray,$count,$start,$stop,$configsection = $null
     $filecontent = (Get-content $file).trim().tolower()
 
     # Create an array to store the line number of each section
     $linetable = @()
-    $objarray = @{}
+    $objarray = [ordered]@{}
+
+    $filecontent = $filecontent | Where-Object {$_ -ne ""}
 
     # Iterate through the file content and store the line number of each section
-    Foreach($line in $filecontent){
-        if (-not [string]::IsNullOrWhiteSpace($line) -and -not $line.startswith("#")){
+    foreach ($line in $filecontent) {
+        if (-not [string]::IsNullOrWhiteSpace($line) -and -not $line.startswith("#")) {
             # If the line is a section, store the line number
             if ($line -match "^\[([^\]]+)\]$") {
                 # Store the section name and line number. Remove the brackets from the section name
                 $linetable += $filecontent.indexof($line)
-
             }
         }
     }
 
     # Iterate through the line numbers and extract the section content
     $count = 0
-    foreach($entry in $linetable){
-
+    foreach ($entry in $linetable) {
         # Get the section name
         $name = $filecontent[$entry]
         # Remove the brackets from the section name
-        $name = $name.replace("[","").replace("]","")
+        $name = $name.replace("[", "").replace("]", "")
 
         # Get the start and stop line numbers for the section content
         # If the section is the last one, set the stop line number to the end of the file
         $start = $entry + 1
-        if($count -eq ($linetable.length-1)){
-            $stop = $filecontent.length - 1
+
+        if($linetable.count -eq $count+1){
+            $stop = $filecontent.count - 1
+        }else{
+            $stop = $linetable[$count + 1] -1
         }
-        else{
-            $stop = $linetable[$count+1] - 2
-        }
+
 
         # Extract the section content
         $configsection = $filecontent[$start..$stop]
 
         # Add the section content to the object array
-        $objarray += @{$Name=$configsection}
+        $objarray += @{$name = $configsection}
 
         # Increment the count
         $count++
     }
 
-    # Return the object array and cast to pscustomobject
+    # Return the object array and cast to PSCustomObject
     return [pscustomobject]$objarray
-
-  }
+}
 
   function Get-ResourceGroupsByList {
     param (
