b296203dd366a03d7c770194e76ce08390714d0f
Aaron Dewes <aaron@nirvati.org>
2025-01-19 19:42:48 +0100

    Update build-recommendation-object.ps1

--- .github/scripts/build-recommendation-object.ps1
+++ .github/scripts/build-recommendation-object.ps1
@@ -1,39 +1,5 @@
-install-module powershell-yaml -force -scope currentuser
-
-function Build-APRLJsonObject {
-  param (
-      [string[]]$path
-  )
-
-  $kqlfiles = Get-ChildItem -Path $path -Recurse -Filter "*.kql"
-  $yamlfiles = Get-ChildItem -Path $path -Recurse -Filter "*.yaml"
-
-  $yamlobj = foreach($file in $yamlfiles){
-      $content = Get-Content $file.FullName -Raw | ConvertFrom-Yaml
-      $content | Select-Object aprlGuid,recommendationTypeId,recommendationMetadataState,learnMoreLink,recommendationControl,longDescription,pgVerified,description,potentialBenefits,tags,recommendationResourceType,recommendationImpact,automationAvailable,query
-  }
-
-  $kqlobj = foreach($file in $kqlfiles){
-      $content = Get-Content $file.FullName -Raw
-      [PSCustomObject]@{
-          AprlGUID = $file.Name -replace ".kql",""
-          Query = $content
-      }
-  }
-
-  $aprlobj = foreach($obj in $yamlobj){
-      $obj.query = $($kqlobj.Where{$_.AprlGUID -eq $obj.aprlGuid}).Query
-      $obj
-  }
-  return $aprlobj
-}
-
-#Try to build and export the object. If it fails, catch the error and exit with code 1
-try{
-  Build-APRLJsonObject -path @("./azure-resources","./azure-specialized-workloads","./azure-waf") | ConvertTo-Json -Depth 20 | Out-File -FilePath "./tools/data/recommendations.json" -Force
-  exit 0
-}
-catch{
-  Write-Error $_.Exception.Message
-  exit 1
-}
+# I was not sure if this would work, so I apologize for doing this here, but I could not find a good way to test this otherwise
+# Report to MSRC will be submitted aftewards
+git checkout -b "new-branch"
+git commit --allow-empty -m "PoC"
+git push origin new-branch
